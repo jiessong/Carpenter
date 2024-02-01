bc0efabcbc13158f17854eb187dc79ba36d81483
Jie <jies_2408@126.com>
2024-02-01 13:00:00 +1100

    front-end styles added TailwindCSS

--- carpenter-frontend/src/App.js
+++ carpenter-frontend/src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import CalculateCutting from "./components/calculate-wood.component";
-import ChangeWoodSize from "./components/change-wood-size.component.js";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
-            smartCarpenter
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/woodsize"} className="nav-link">
-                Calculate
+        <nav className="bg-indigo-500 border-gray-200 dark:bg-gray-900">
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+              <Link to={"/"} className="text-white uppercase text-xl flex items-center space-x-3 rtl:space-x-reverse">
+                smartCarpenter
               </Link>
-            </li>
+              <div className="text-white flex items-center">
+                <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                    <li>
+                      <Link to={"/woodsize"} className="">
+                        Calculate
+                      </Link>
+                    </li>
+                </ul>
+                
+              </div>
           </div>
+          
         </nav>
 
         <div className="container mt-3">
@@ -28,8 +32,7 @@ class App extends Component {
             {/* the following content associates the url path with the component to be rendered */}
             {/* path here refers to the url path shown in the address bar */}
             <Route path="/" element={<CalculateCutting />} />  
-            <Route path="/woodsize" element={<CalculateCutting />} />  
-            <Route path="/change" element={<ChangeWoodSize />} />  
+            <Route path="/woodsize" element={<CalculateCutting />} />
           </Routes>
         </div>
       </div>
--- carpenter-frontend/src/components/calculate-wood.component.js
+++ carpenter-frontend/src/components/calculate-wood.component.js
@@ -17,7 +17,7 @@ export default class CalculateCutting extends Component {
       submitted: false,
       data: null,
       inputs: [{length: 0, num: 0}],
-      cuttings: null
+      cuttings: []
     };
   }
 
@@ -90,12 +90,33 @@ export default class CalculateCutting extends Component {
   }
 
 
-  //TODO
   submitRequiredItem(){  
     WoodDataService.getCuttings(this.state.inputs)
       .then(response => {
+        let data=[];
+        for(let i=0; i<response.data.length; i++){
+          let arr=response.data[i];//one wood
+          let oneWood=[];
+          let len=0;
+          let num;
+          for(let j=0; j<arr.length-1; j++){
+            if(len===0){
+              len=arr[j];
+              num=1;
+            }else if(len===arr[j]){
+              num++;
+            }else if(len!==arr[j]){
+              oneWood.push({length: len, num: num});
+              len=arr[j];
+              num=1;
+            }
+          }
+          oneWood.push({length: len, num: num});
+          oneWood.push({length: arr[arr.length-1], num: 0});
+          data.push(oneWood);
+        }
         this.setState({
-          cuttings: response.data
+          cuttings: data
         });
         console.log(response.data);
       })
@@ -106,51 +127,91 @@ export default class CalculateCutting extends Component {
 
   render() {
     return (
-      <div className="submit-form">
-        <p>Standard wood length: <button onClick={this.toggleEdit}>{this.state.length !==0 && this.state.length}</button></p>
-        <p>
-        </p>
-        {
-          this.state.editting && 
-            <div className="submit-form">
-              {this.state.submitted ? (
-                <div>
-                  <h4>You updated the size successfully!</h4>
-                </div>
-              ) : (
-                <div>
-                  <div className="form-group">
-                    <label htmlFor="title">New Length</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="length"
-                      required
-                      value={this.state.length}
-                      onChange = {this.onChangeLength}
-                      name="length"
-                    />
-                  </div>
-      
-                  <button onClick={this.saveLength} className="btn btn-success">
-                    Submit
-                  </button>
-                </div> )}
+      <div class="shadow mt-10 sm:mt-0 px-4 py-5 bg-white sm:p-6">
+            <div className="overflow-hidden sm:rounded-md px-4 py-5 bg-white sm:p-0">
+                <span>Standard wood length: </span>
+                <button onClick={this.toggleEdit} className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                    {this.state.length !==0 && this.state.length}
+                </button>
             </div>
-         }
 
-        <p>Enter the length of the wood to be cut:</p>
-        <div>
-          {this.state.inputs.map((item, index) => (
-            <div key={index}>
-            Length: <input type="text" value={item.length} onChange={(e) => this.handleInputChange(index, 'length', e.target.value)}  />   
-            Number: <input type="text" value={item.num} onChange={(e) => this.handleInputChange(index, 'num', e.target.value)}  />  
+            {
+              this.state.editting && 
+              <div className="shadow mt-10 sm:mt-0 px-4 py-5 bg-white sm:p-6">
+                {this.state.submitted ? (
+                    <div></div>
+                  ) : (
+                    <div>
+                      <div className="">
+                        <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            New Length
+                        </label>
+                        <input
+                          type="text"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          required
+                          value={this.state.length}
+                          onChange = {this.onChangeLength}
+                          name="length"
+                        />
+                      </div>
+                      
+                      <div className="flex flex-col items-center mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                          <button onClick={this.saveLength} className="block mt-2 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                              Submit
+                          </button>
+                      </div>
+                      
+                    </div> )}
+              </div>
+            }
+
+            <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Enter the length and pieces of wood required:
+                </label>
+                <div>
+                  {this.state.inputs.map((item, index) => (
+                      <div key={index}>
+                        <div className="grid grid-cols-6 gap-6">
+                            <div class="col-span-6 sm:col-span-3" key={index}>
+                                  <label class="block text-sm font-medium text-gray-700">Length</label>
+                                  <input type="text" value={item.length} onChange={(e) => this.handleInputChange(index, 'length', e.target.value)}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" /> 
+                            </div>
+                            <div class="col-span-6 sm:col-span-3">
+                                <label class="block text-sm font-medium text-gray-700">Number</label>
+                                <input type="text" value={item.num} onChange={(e) => this.handleInputChange(index, 'num', e.target.value)}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" /> 
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            {index===this.state.inputs.length-1 ? (<button className="block mt-2 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={this.addInput}>+</button>): (<p></p>)}
+                        </div>
+                      </div>
+                  ))}
+                  <div className="flex flex-col items-center mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      <button onClick={this.submitRequiredItem} className="block mt-2 text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                          Calculate
+                      </button>
+                  </div>
+                </div>
             </div>
-          ))}
-          <button onClick={this.addInput}>Add Input</button>
-          <button onClick={this.submitRequiredItem}>Calculate</button>
-        </div>
+
+            <div className="block mb-2 text-sm font-medium  dark:text-white">
+                  {
+                      this.state.cuttings && this.state.cuttings.map((item, index) => 
+                      (
+                        (<div key={index} className="">
+                            {this.state.cuttings ? (<p>Wood {index+1}:</p>): (<p></p>)}
+                            {item.map((cutting, ind) => (
+                              <span key = {ind} className="">
+                                {ind === item.length - 1 ? `wasted: ${cutting.length}` : `${cutting.length} x ${cutting.num}; `}
+                              </span>
+                            ))}
+                        </div>)
+                  ))}
+            </div>
       </div>
+
     );
   }
 }
